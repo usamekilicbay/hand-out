3856944578ffc8d1e0a36e1034c78604a02f8839
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-19 23:10:01 +0300

    refactor(Startup): add UseStaticFiles

--- hand-out/Startup.cs
+++ hand-out/Startup.cs
@@ -27,6 +27,8 @@ namespace hand_out
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
