f62f1af2573e2ea2b223f6ce9becd32ed109f287
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-24 11:11:50 +0300

    feat: create Sidekick.NET which includes Constant, EnumExtension, Types classes

---
+++ Sidekick.NET/Types.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidekick.NET
+{
+    public static class Types
+    {
+        public enum UserStatus
+        {
+            PASSIVE,
+            ACTIVE
+        }
+
+        public enum UserActivityStatus
+        {
+            OFFLINE,
+            ONLINE
+        }
+
+        public enum SessionRole
+        {
+            NON,
+            STUDENT,
+            TEACHER,
+            ADMIN
+        }
+
+        public enum SessionOperation
+        {
+            START,
+            END
+        }
+    }
+}
