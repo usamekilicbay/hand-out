f910d8df7f5f61527037c790b57d4cf08d5379be
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-24 11:12:18 +0300

    feat: add User.cs model

---
+++ hand-out/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hand_out.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string ProfilePhotoLink { get; set; }
+        public string Adress { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastSeen { get; set; }
+    }
+}
