84bae3c9660ee16ad198f8f67dd97cecfbda4dcf
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-25 16:21:36 +0300

    feat: add Product.cs model

---
+++ hand-out/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hand_out.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Address { get; set; }
+        public short Status { get; set; }
+        public User Grantor { get; set; }
+        public User Needer { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
