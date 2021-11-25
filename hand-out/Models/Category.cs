e7464253b3dc3f539dd71efa4d978c21e0bf2ded
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-25 16:24:58 +0300

    feat: add Category.cs model

---
+++ hand-out/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hand_out.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
