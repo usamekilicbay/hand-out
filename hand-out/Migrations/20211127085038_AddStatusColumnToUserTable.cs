4108614b0bbfd1946a4c27e7b906a42551cbc99d
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-27 12:08:44 +0300

    chore: add StatusColumnToUserTable migration

---
+++ hand-out/Migrations/20211127085038_AddStatusColumnToUserTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hand_out.Migrations
+{
+    public partial class AddStatusColumnToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Status",
+                table: "User",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "User");
+        }
+    }
+}
