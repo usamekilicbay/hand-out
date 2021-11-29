e4ac60e706c9f6fdce0991ab3e9beff87041430d
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-11-29 13:20:24 +0300

    feat: add Admin controller

---
+++ hand-out/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using hand_out.Data;
+using hand_out.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hand_out.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            List<User> users = _db.User.ToList();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateResult(User user)
+        {
+            _db.User.Add(user);
+            _db.SaveChanges();
+
+            return Redirect("GetUsers");
+        }
+    }
+}
