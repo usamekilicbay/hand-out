880ff67fdab8f80a6b0b58c614c0b02718e9458c
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-01 14:43:50 +0300

    feat: add IRepository in Abstract folder

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+
+        void Create(T p);
+
+        void Update(T p);
+
+        void Delete(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
