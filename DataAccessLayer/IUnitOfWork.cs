081805b549fa3292969c521f50ca34c6aa037473
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-10 13:27:02 +0300

    refactor: add UnitOfWork and IUnitOfWork to DataAccessLayer

---
+++ DataAccessLayer/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Repositories.Abstract;
+using System;
+
+namespace DataAccessLayer
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; }
+        IProductRepository ProductRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+
+        int Complete();
+    }
+}
