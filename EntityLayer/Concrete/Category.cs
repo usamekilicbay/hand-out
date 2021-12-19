1c9cdef977b5144669efa7e35bc3fd958c916b7b
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-19 22:30:06 +0300

    refactor: inherit BaseEntity, remove ID, Name, CreationDate in Category entity

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using static Sidekick.NET.Types;
 
 namespace EntityLayer.Concrete
 {
-    public class Category
+    public class Category: BaseEntity
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public DateTime CreationDate { get; set; }
         public CategoryStatus Status { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
