7f3cc719ba456b99ef8e9815ea35300a2e6a558e
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-31 13:12:57 +0300

    feat: add CreateUserViewModel and UpdateUserViewModel

---
+++ hand-out/Areas/AdminPanel/Models/ViewModels/User/CreateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Sidekick.NET.Types;
+
+namespace hand_out.Areas.AdminPanel.Models.ViewModels.User
+{
+    public class CreateUserViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        [Display(Name = "Profile Photo URL")]
+        public string ProfilePhotoURL { get; set; }
+        public UserStatus Status { get; set; }
+    }
+}
---
+++ hand-out/Areas/AdminPanel/Models/ViewModels/User/UpdateUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using static Sidekick.NET.Types;
+
+namespace hand_out.Areas.AdminPanel.Models.ViewModels.User
+{
+    public class UpdateUserViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        [Display(Name = "Profile Photo URL")]
+        public string ProfilePhotoURL { get; set; }
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+        [Display(Name = "Date Modified")]
+        public DateTime DateModified { get; set; }
+        [Display(Name = "Last Seen")]
+        public DateTime LastSeen { get; set; }
+        public UserStatus Status { get; set; }
+    }
+}
