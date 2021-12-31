66e8e812b0214c3e1221c44eec00355138d2324f
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-31 13:14:59 +0300

    feat: add CreateUserViewModelValitador and UpdateUserViewModelValitador

---
+++ hand-out/Areas/AdminPanel/Models/Validators/User/UpdateUserViewModelValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using hand_out.Areas.AdminPanel.Models.ViewModels.User;
+using static Sidekick.NET.Constant.Validation.Rule.User;
+
+namespace hand_out.Areas.AdminPanel.Models.Validators.User
+{
+    public class UpdateUserViewModelValidator : AbstractValidator<UpdateUserViewModel>
+    {
+        public UpdateUserViewModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .Length(Name.MIN_LENGTH, Name.MAX_LENGTH);
+
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(Email.MAX_LENGTH);
+
+            RuleFor(x => x.Address)
+                .MaximumLength(Address.MAX_LENGTH);
+
+            RuleFor(x => x.ProfilePhotoURL)
+                .MaximumLength(ProfilePhotoURL.MAX_LENGTH);
+        }
+    }
+}
