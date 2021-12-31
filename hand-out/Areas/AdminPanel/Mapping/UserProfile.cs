e2ad76d55e1fd06568423e0db5c971f3653f5d9c
Usame Kilicbay <usame.kilicbay@gmail.com>
2021-12-31 13:15:56 +0300

    feat: add UserProfile to Mapping

---
+++ hand-out/Areas/AdminPanel/Mapping/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using hand_out.Areas.AdminPanel.Models.ViewModels.User;
+
+namespace hand_out.Areas.AdminPanel.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, CreateUserViewModel>()
+                .ReverseMap();
+
+            CreateMap<User, UpdateUserViewModel>()
+                .ReverseMap();
+        }
+    }
+}
