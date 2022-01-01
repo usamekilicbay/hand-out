468921b548383ead1bf0ea815ded0e8da10ffd99
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 12:34:14 +0300

    feat: add CategoryProfile to Mapping

---
+++ hand-out/Areas/AdminPanel/Mapping/CategoryProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using hand_out.Areas.AdminPanel.Models.ViewModels.Category;
+
+namespace hand_out.Areas.AdminPanel.Mapping
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, ListCategoryViewModel>()
+               .ReverseMap();
+
+            CreateMap<Category, CreateCategoryViewModel>()
+                .ReverseMap();
+
+            CreateMap<Category, UpdateCategoryViewModel>()
+                .ReverseMap();
+        }
+    }
+}
