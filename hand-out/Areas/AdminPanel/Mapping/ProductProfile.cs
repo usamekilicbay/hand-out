ecabfb27f14bbc0835f1d93ef852757b739162e4
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 12:29:59 +0300

    feat: add ProductProfile to Mapping

---
+++ hand-out/Areas/AdminPanel/Mapping/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using hand_out.Areas.AdminPanel.Models.ViewModels.Product;
+
+namespace hand_out.Areas.AdminPanel.Mapping
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ListProductViewModel>()
+                .ReverseMap();
+
+            CreateMap<Product, UpdateProductViewModel>()
+                .ReverseMap();
+        }
+    }
+}
