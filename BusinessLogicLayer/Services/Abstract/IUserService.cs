e8650331694111de095d7fad5eac6457827b3686
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 18:37:29 +0300

    feat: add IUserService, UserService

---
+++ BusinessLogicLayer/Services/Abstract/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using EntityLayer.Concrete;
+
+namespace BusinessLogicLayer.Services.Abstract
+{
+    public interface IUserService : IService<User>
+    {
+    }
+}
---
+++ BusinessLogicLayer/Services/Concrete/UserService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Services.Abstract;
+using DataAccessLayer;
+using DataAccessLayer.Repositories.Concrete;
+using EntityLayer.Concrete;
+
+namespace BusinessLogicLayer.Services.Concrete
+{
+    public class UserService : Service<User>, IUserService
+    {
+        public UserService(ApplicationDbContext applicationDbContext, IMapper mapper) : base(mapper)
+        {
+            Repository = new UserRepository(applicationDbContext);
+        }
+    }
+}
