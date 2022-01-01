c56bce7e2b0ddc47455202404dc341245160ad3e
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 18:44:55 +0300

    feat: add IUnitOfWork, UnitOfWork

---
+++ BusinessLogicLayer/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using BusinessLogicLayer.Services.Abstract;
+
+namespace BusinessLogicLayer
+{
+    public interface IUnitOfWork
+    {
+        public ICategoryService CategoryService { get; }
+        public IProductService ProductService { get; }
+        public IUserService UserService { get; }
+    }
+}
---
+++ BusinessLogicLayer/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Services.Abstract;
+using BusinessLogicLayer.Services.Concrete;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICategoryService CategoryService { get; private set; }
+
+        public IProductService ProductService { get; private set; }
+
+        public IUserService UserService { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            CategoryService = new CategoryService(applicationDbContext, mapper);
+            ProductService = new ProductService(applicationDbContext, mapper);
+            UserService = new UserService(applicationDbContext, mapper);
+        }
+    }
+}
