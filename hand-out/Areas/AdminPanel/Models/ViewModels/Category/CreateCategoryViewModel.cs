cfbdd9f16448776cfa2495537c18e076141c217f
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 12:32:46 +0300

    feat: add ListCategoryViewModel, CreateCategoryViewModel, UpdateCategoryViewModel

---
+++ hand-out/Areas/AdminPanel/Models/ViewModels/Category/CreateCategoryViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace hand_out.Areas.AdminPanel.Models.ViewModels.Category
+{
+    public class CreateCategoryViewModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ hand-out/Areas/AdminPanel/Models/ViewModels/Category/ListCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using static Sidekick.NET.Types;
+
+namespace hand_out.Areas.AdminPanel.Models.ViewModels.Category
+{
+    public class ListCategoryViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+        [Display(Name = "Date Modified")]
+        public DateTime DateModified { get; set; }
+        public CategoryStatus Status { get; set; }
+    }
+}
