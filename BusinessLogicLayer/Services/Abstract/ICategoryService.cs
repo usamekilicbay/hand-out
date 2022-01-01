071860f42c23daecd74780896b06a5209185dd7f
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-01 18:36:41 +0300

    feat: add ICategoryService, CategoryService

---
+++ BusinessLogicLayer/Services/Abstract/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer.Services.Abstract
+{
+    public interface ICategoryService : IService<Category>
+    {
+        public List<string> GetCategoryNames();
+    }
+}
---
+++ BusinessLogicLayer/Services/Concrete/CategoryService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Services.Abstract;
+using DataAccessLayer;
+using DataAccessLayer.Repositories.Concrete;
+using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer.Services.Concrete
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        public CategoryService(ApplicationDbContext applicationDbContext, IMapper mapper) : base(mapper)
+        {
+            Repository = new CategoryRepository(applicationDbContext);
+        }
+
+        public List<string> GetCategoryNames()
+        {
+            List<Category> categories = Repository.GetAll();
+            List<string> categoryNames = new List<string>();
+
+            foreach (Category category in categories)
+                categoryNames.Add(category.Name);
+
+            return categoryNames;
+        }
+    }
+}
