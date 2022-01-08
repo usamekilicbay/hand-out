c7228fa4a97379804d9eef0c521f5792370e13e8
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:45:30 +0300

    refactor: add IProductRepository property to IProductService

--- BusinessLogicLayer/Services/Abstract/IProductService.cs
+++ BusinessLogicLayer/Services/Abstract/IProductService.cs
@@ -1,8 +1,10 @@
-﻿using EntityLayer.Concrete;
+﻿using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
 
 namespace BusinessLogicLayer.Services.Abstract
 {
     public interface IProductService : IService<Product>
     {
+        public IProductRepository ProductRepository { get; set; }
     }
 }
