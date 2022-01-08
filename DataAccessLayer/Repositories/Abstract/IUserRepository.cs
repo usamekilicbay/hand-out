d795bebcb4b6aad4b0b3fd5ff6a5d51b92a6dc58
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:57:16 +0300

    refactor: add UserManager and SignInManager properties, write GetUserDetails, SignUp, PasswordSignIn method signs in IUserRepository

--- DataAccessLayer/Repositories/Abstract/IUserRepository.cs
+++ DataAccessLayer/Repositories/Abstract/IUserRepository.cs
@@ -1,8 +1,16 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace DataAccessLayer.Repositories.Abstract
 {
     public interface IUserRepository : IRepository<User>
     {
+        public UserManager<User> UserManager { get; set; }
+        public SignInManager<User> SignInManager { get; set; }
+
+        public User GetUserDetails(ClaimsPrincipal id);
+        public SignInResult PasswordSignIn(string userName, string password, bool isPersistent, bool lockoutOnFailure = false);
+        public IdentityResult SignUp(User user, string password);
     }
 }
