ce979404c2c432479fc1549e337f765ed16e7c55
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 15:52:10 +0300

    feat: add DetailsUserViewModel

---
+++ hand-out/Models/ViewModels/User/DetailsUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+using static Sidekick.NET.Types;
+
+namespace hand_out.Models.ViewModels.User
+{
+    public class DetailsUserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ProfilePhotoURL { get; set; }
+        public UserStatus Status { get; set; }
+
+        public virtual IList<Product> Products { get; set; }
+    }
+}
