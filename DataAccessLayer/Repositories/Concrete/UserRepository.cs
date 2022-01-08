8843f0faa06a22b9ead1ad0016fcf821f4ab12b2
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 15:00:49 +0300

    refactor: implement and fill UserManager and SignInManager properties and GetUserDetails, SignUp, PasswordSignIn methods and pass UserManager, SignInManager parameters to Constructor in UserRepository

--- DataAccessLayer/Repositories/Concrete/UserRepository.cs
+++ DataAccessLayer/Repositories/Concrete/UserRepository.cs
@@ -1,12 +1,36 @@
 ﻿using DataAccessLayer.Repositories.Abstract;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace DataAccessLayer.Repositories.Concrete
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
-        public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+        public UserManager<User> UserManager { get; set; }
+        public SignInManager<User> SignInManager { get; set; }
+
+        public UserRepository(ApplicationDbContext dbContext,
+            UserManager<User> userManager, SignInManager<User> signInManager) : base(dbContext)
         {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public User GetUserDetails(ClaimsPrincipal claimsPrincipal)
+        {
+            return UserManager.FindByIdAsync(UserManager.GetUserId(claimsPrincipal)).Result;
+        }
+
+        public SignInResult PasswordSignIn(string userName, string password, bool isPersistent, bool lockoutOnFailure = false)
+        {
+            return SignInManager.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure).Result;
+        }
+
+        public IdentityResult SignUp(User user, string password)
+        {
+            return UserManager.CreateAsync(user, password).Result;
         }
     }
 }
