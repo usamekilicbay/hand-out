3f038de3c195d49f2bfb03200176a40e5b406652
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 15:39:16 +0300

    feat: add SignInUserViewModel

---
+++ hand-out/Models/ViewModels/User/SignInUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace hand_out.Models.ViewModels.User
+{
+    public class SignInUserViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
