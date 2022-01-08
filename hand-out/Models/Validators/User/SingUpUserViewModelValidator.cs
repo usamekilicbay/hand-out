bc5de8ea23745cc63ccf4bd6668a1013227de035
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 15:48:44 +0300

    feat: add SignUpUserViewModelValitador

---
+++ hand-out/Models/Validators/User/SingUpUserViewModelValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using hand_out.Models.ViewModels.User;
+using Sidekick.NET.Constant.Validation;
+using static Sidekick.NET.Constant.Validation.Rule.User;
+
+namespace hand_out.Models.Validators.User
+{
+    public class SingUpUserViewModelValidator : AbstractValidator<SignUpUserViewModel>
+    {
+        public SingUpUserViewModelValidator()
+        {
+            RuleFor(u => u.UserName)
+                .NotNull()
+                .NotEmpty()
+                .Length(Name.MIN_LENGTH, Name.MAX_LENGTH);
+
+            RuleFor(u => u.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(Email.MAX_LENGTH);
+        }
+    }
+}
