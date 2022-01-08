5132a62f7712648edd905e81499cb70e250c03af
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:41:02 +0300

    refactor: add ICategoryRepository property to ICagoryService

--- BusinessLogicLayer/Services/Abstract/ICategoryService.cs
+++ BusinessLogicLayer/Services/Abstract/ICategoryService.cs
@@ -1,10 +1,12 @@
-﻿using EntityLayer.Concrete;
+﻿using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
 using System.Collections.Generic;
 
 namespace BusinessLogicLayer.Services.Abstract
 {
     public interface ICategoryService : IService<Category>
     {
+        public ICategoryRepository CategoryRepository { get; set; }
         public List<string> GetCategoryNames();
     }
 }
