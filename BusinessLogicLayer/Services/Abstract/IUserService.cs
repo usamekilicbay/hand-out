f78c5b0704100fcd278cb1aa8df4be9a04c7a1c2
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:51:56 +0300

    refactor: add IUserRepository property and GetUserDetails, SignUp, PasswordSignIn method signs to IUserService

--- BusinessLogicLayer/Services/Abstract/IUserService.cs
+++ BusinessLogicLayer/Services/Abstract/IUserService.cs
@@ -1,8 +1,16 @@
-﻿using EntityLayer.Concrete;
+﻿using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace BusinessLogicLayer.Services.Abstract
 {
     public interface IUserService : IService<User>
     {
+        public IUserRepository UserRepository { get; set; }
+
+        public ViewModel GetUserDetails<ViewModel>(ClaimsPrincipal claimsPrincipal);
+        public IdentityResult SignUp<ViewModel>(ViewModel viewModel, string password);
+        public SignInResult PasswordSignIn(string userName, string password, bool isPersistent, bool lockoutOnFailure = false);
     }
 }
