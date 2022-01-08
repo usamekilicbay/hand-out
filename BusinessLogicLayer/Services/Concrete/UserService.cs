0040c3f7d48a1d23075200028a8a5d44758083f5
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:54:00 +0300

    refactor: implement and fill IUserRepository property and GetUserDetails, SignUp, PasswordSignIn methods and pass UserManager, SignInManager parameters to Constructor in UserService

--- BusinessLogicLayer/Services/Concrete/UserService.cs
+++ BusinessLogicLayer/Services/Concrete/UserService.cs
@@ -1,16 +1,43 @@
 ﻿using AutoMapper;
 using BusinessLogicLayer.Services.Abstract;
 using DataAccessLayer;
+using DataAccessLayer.Repositories.Abstract;
 using DataAccessLayer.Repositories.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace BusinessLogicLayer.Services.Concrete
 {
     public class UserService : Service<User>, IUserService
     {
-        public UserService(ApplicationDbContext applicationDbContext, IMapper mapper) : base(mapper)
+        public IUserRepository UserRepository { get; set; }
+
+        public UserService(ApplicationDbContext applicationDbContext, IMapper mapper,
+            UserManager<User> userManager, SignInManager<User> signInManager)
+            : base(mapper)
         {
-            Repository = new UserRepository(applicationDbContext);
+            UserRepository = new UserRepository(applicationDbContext, userManager, signInManager);
+            Repository = UserRepository;
+        }
+
+        public ViewModel GetUserDetails<ViewModel>(ClaimsPrincipal claimsPrincipal)
+        {
+            User user = UserRepository.GetUserDetails(claimsPrincipal);
+
+            return mapper.Map<ViewModel>(user);
+        }
+
+        public SignInResult PasswordSignIn(string userName, string password, bool isPersistent, bool islockoutOnFailure)
+        {
+            return UserRepository.PasswordSignIn(userName, password, isPersistent, islockoutOnFailure);
+        }
+
+        public IdentityResult SignUp<ViewModel>(ViewModel viewModel, string password)
+        {
+            User user = mapper.Map<User>(viewModel);
+
+            return UserRepository.SignUp(user, password);
         }
     }
 }
