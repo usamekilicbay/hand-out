48904c4e3e6c2b3bdb9befb5ca207b8853715e0d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-08 14:42:52 +0300

    refactor: implement ICategoryRepository property and create instance of CategoryRepository in CagoryService

--- BusinessLogicLayer/Services/Concrete/CategoryService.cs
+++ BusinessLogicLayer/Services/Concrete/CategoryService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BusinessLogicLayer.Services.Abstract;
 using DataAccessLayer;
+using DataAccessLayer.Repositories.Abstract;
 using DataAccessLayer.Repositories.Concrete;
 using EntityLayer.Concrete;
 using System.Collections.Generic;
@@ -9,15 +10,18 @@ namespace BusinessLogicLayer.Services.Concrete
 {
     public class CategoryService : Service<Category>, ICategoryService
     {
+        public ICategoryRepository CategoryRepository { get; set; }
+
         public CategoryService(ApplicationDbContext applicationDbContext, IMapper mapper) : base(mapper)
         {
-            Repository = new CategoryRepository(applicationDbContext);
+            CategoryRepository = new CategoryRepository(applicationDbContext);
+            Repository = CategoryRepository;
         }
 
         public List<string> GetCategoryNames()
         {
             List<Category> categories = Repository.GetAll();
-            List<string> categoryNames = new List<string>();
+            List<string> categoryNames = new();
 
             foreach (Category category in categories)
                 categoryNames.Add(category.Name);
