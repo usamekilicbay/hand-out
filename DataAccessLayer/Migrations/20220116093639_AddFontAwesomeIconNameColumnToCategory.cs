bdf5ccf9e88fe699fe79ba537883e4b77137ee21
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-16 13:27:20 +0300

    refactor: add migration for Category Entity update

---
+++ DataAccessLayer/Migrations/20220116093639_AddFontAwesomeIconNameColumnToCategory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AddFontAwesomeIconNameColumnToCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FontAwesomeIconName",
+                table: "Category",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FontAwesomeIconName",
+                table: "Category");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20220116102349_ChangeFontAwesomeIconNameInCategory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class ChangeFontAwesomeIconNameInCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FontAwesomeIconName",
+                table: "Category",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FontAwesomeIconName",
+                table: "Category",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
