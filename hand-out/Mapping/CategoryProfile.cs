565cbb3cb2176bc0bc18c0b82927091e1efd7146
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-16 15:35:52 +0300

    refactor: create CategoryProfile

---
+++ hand-out/Mapping/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using hand_out.Models.ViewModels.Category;
+
+namespace hand_out.Mapping
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, ListCategoryViewModel>()
+                .ReverseMap();
+        }
+    }
+}
