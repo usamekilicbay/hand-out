41bf07dc0111dd55d340bea6c9d6529cbaded3f2
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-16 15:36:13 +0300

    refactor: create ProductProfile

---
+++ hand-out/Mapping/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using hand_out.Models.ViewModels.Product;
+
+namespace hand_out.Mapping
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ListProductViewModel>()
+                .ReverseMap();
+
+            CreateMap<Product, CreateProductViewModel>()
+                .ReverseMap();
+        }
+    }
+}
