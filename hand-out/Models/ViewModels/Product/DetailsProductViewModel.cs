fbee5024c94b37d3d42fc327f775f3ffcb97953a
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-21 00:49:09 +0300

    feat: create DetailsProductViewModel

---
+++ hand-out/Models/ViewModels/Product/DetailsProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hand_out.Models.ViewModels.Product
+{
+    public class DetailsProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Details { get; set; }
+        public string GrantorName { get; set; }
+        public string CategoryName { get; set; }
+        public string GrantorId { get; set; }
+        public DateTime DatePublished { get; set; }
+        public List<string> PhotoLinks { get; private set; }
+
+        public DetailsProductViewModel(string photoLink)
+        {
+            PhotoLinks = photoLink.Split('|').ToList();
+        }
+    }
+}
