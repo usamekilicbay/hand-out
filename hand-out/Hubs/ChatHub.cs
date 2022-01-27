5a12f26ef8ef9729bebe25606b60644eee6c1f9c
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-01-27 22:11:28 +0300

    feat: add ChatHub class

---
+++ hand-out/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hand_out.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async void SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
