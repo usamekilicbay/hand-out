44d5dadcf18a9f63f48dab871f77f6e71d52db6c
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-04 18:49:18 +0300

    refactor: multiple important changes in ApplicationDbContext

--- DataAccessLayer/ApplicationDbContext.cs
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Sidekick.NET.Constant.SQL;
 using Sidekick.NET.Constant.Validation;
-using static Sidekick.NET.Types;
+using System;
 
 namespace DataAccessLayer
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -15,8 +15,9 @@ namespace DataAccessLayer
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             SetCategory(modelBuilder);
+            SetMessage(modelBuilder);
             SetProduct(modelBuilder);
-            SetUserTable(modelBuilder);
+            SetUser(modelBuilder);
 
             base.OnModelCreating(modelBuilder);
         }
@@ -33,6 +34,11 @@ namespace DataAccessLayer
                 .IsRequired();
 
             modelBuilder.Entity<Category>()
+                .Property(c => c.FontAwesomeIconName)
+                .HasMaxLength(Rule.Category.FontAwesomeIconName.MAX_LENGTH)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
                 .Property(c => c.DateCreated)
                 .HasColumnType(Rule.Category.DateCreated.DATA_TYPE)
                 .HasDefaultValueSql(Rule.Category.DateCreated.DEFAULT_VALUE_SQL);
@@ -47,6 +53,28 @@ namespace DataAccessLayer
                 .HasColumnType(Rule.Category.Status.DATA_TYPE)
                 .HasDefaultValue(Rule.Category.Status.DEFAULT_VALUE)
                 .IsRequired();
+        }
+
+        private static void SetMessage(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>()
+                .Property(m => m.Id)
+                .HasDefaultValueSql(Rule.Message.Id.DEFAULT_VALUE_SQL);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Sender)
+                .WithMany(u => u.SentMessages)
+                .HasForeignKey(m => m.SenderId);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Receiver)
+                .WithMany(u => u.ReceivedMessages)
+                .HasForeignKey(m => m.ReceiverId);
+
+            modelBuilder.Entity<Message>()
+                .Property(m => m.DateSent)
+                .HasColumnType(Rule.Message.DateSent.DATA_TYPE)
+                .HasDefaultValueSql(Rule.Message.DateSent.DEFAULT_VALUE_SQL);
         }
 
         private static void SetProduct(ModelBuilder modelBuilder)
@@ -107,26 +135,12 @@ namespace DataAccessLayer
                 .IsRequired();
         }
 
-        private static void SetUserTable(ModelBuilder modelBuilder)
+        private static void SetUser(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>()
-                .HasKey(u => u.ID);
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.Name)
-                .HasMaxLength(Rule.User.Name.MAX_LENGTH)
-                .IsUnicode()
-                .IsRequired();
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.Email)
-                .HasMaxLength(Rule.User.Email.MAX_LENGTH)
-                .IsUnicode();
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.Address)
-                .HasMaxLength(Rule.User.Address.MAX_LENGTH)
-                .IsUnicode();
+              .Property(u => u.Address)
+              .HasMaxLength(Rule.User.Address.MAX_LENGTH)
+              .IsUnicode();
 
             modelBuilder.Entity<User>()
                 .Property(u => u.ProfilePhotoURL)
@@ -134,29 +148,29 @@ namespace DataAccessLayer
                 .IsUnicode();
 
             modelBuilder.Entity<User>()
-                .Property(u => u.LastSeen)
-                .HasColumnType(Rule.User.LastSeen.DATA_TYPE)
-                .HasDefaultValueSql(Rule.User.LastSeen.DEFAULT_VALUE_SQL);
-
-            modelBuilder.Entity<User>()
                 .Property(u => u.DateCreated)
                 .HasColumnType(Rule.User.DateCreated.DATA_TYPE)
                 .HasDefaultValueSql(Rule.User.DateCreated.DEFAULT_VALUE_SQL);
 
             modelBuilder.Entity<User>()
-               .Property(u => u.DateModified)
-               .HasColumnType(Rule.User.DateModified.DATA_TYPE)
-               .HasDefaultValueSql(Rule.User.DateModified.DEFAULT_VALUE_SQL);
+                .Property(u => u.DateModified)
+                .HasColumnType(Rule.User.DateModified.DATA_TYPE)
+                .HasDefaultValueSql(Rule.User.DateModified.DEFAULT_VALUE_SQL);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.LastSeen)
+                .HasColumnType(Rule.User.LastSeen.DATA_TYPE)
+                .HasDefaultValueSql(Rule.User.LastSeen.DEFAULT_VALUE_SQL);
 
             modelBuilder.Entity<User>()
                 .Property(u => u.Status)
                 .HasColumnType(Rule.User.Status.DATA_TYPE)
-                .HasDefaultValue(Rule.User.Status.DEFAULT_VALUE)
-                .IsRequired();
+                .HasDefaultValue(Rule.User.Status.DEFAULT_VALUE);
         }
 
-        public DbSet<User> User { get; set; }
-        public DbSet<Product> Product { get; set; }
         public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Message> Message { get; set; }
     }
 }
