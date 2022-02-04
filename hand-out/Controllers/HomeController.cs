9feafb619537e7b3bf1c8b87686272ce36c6d8e6
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-04 13:53:35 +0300

    refactor: add _unitOfWork property, create constructor method and fill Index Action method for HomeController

--- hand-out/Controllers/HomeController.cs
+++ hand-out/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BusinessLogicLayer;
+using DataAccessLayer;
+using hand_out.Models.ViewModels.Category;
+using hand_out.Models.ViewModels.Product;
+using hand_out.Models.ViewModels.Shared;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +14,22 @@ namespace hand_out.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _unitOfWork = new UnitOfWork(applicationDbContext, mapper);
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IndexViewModel indexViewModel = new IndexViewModel
+            {
+                ListProductViewModel = _unitOfWork.ProductService.GetAll<ListProductViewModel>(),
+                ListCategoryViewModel = _unitOfWork.CategoryService.GetAll<ListCategoryViewModel>()
+            };
+
+            return View(indexViewModel);
         }
     }
 }
