ab1ef3e65f2c73316fbf0b71fba6f7bce8d5e921
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-04 18:42:27 +0300

    refactor: add Sender, SenderId, Receiver, ReceiverId properties to Message Entity for table relations

--- EntityLayer/Concrete/Message.cs
+++ EntityLayer/Concrete/Message.cs
@@ -7,5 +7,10 @@ namespace EntityLayer.Concrete
         public string Id { get; set; }
         public string Context { get; set; }
         public DateTime DateSent { get; set; }
+
+        public string SenderId { get; set; }
+        public User Sender { get; set; }
+        public string ReceiverId { get; set; }
+        public User Receiver { get; set; }
     }
 }
