5dd99f9e29e5e9f3236bde2c131d33f24def8d6d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-04 14:12:19 +0300

    feat: add Message Entity

---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Context { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
