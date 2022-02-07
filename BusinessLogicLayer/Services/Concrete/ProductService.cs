0292828738fbf6bdc2fc2070b2c7772b3f411943
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-07 12:02:49 +0300

    refactor: implement InsertAsync method and pass IUnitOfWork as constructor parameter in ProductService

--- BusinessLogicLayer/Services/Concrete/ProductService.cs
+++ BusinessLogicLayer/Services/Concrete/ProductService.cs
@@ -3,7 +3,15 @@ using BusinessLogicLayer.Services.Abstract;
 using DataAccessLayer;
 using DataAccessLayer.Repositories.Abstract;
 using DataAccessLayer.Repositories.Concrete;
+using DataLayer.ViewModels.Product;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Constant = Sidekick.NET.Constant;
 
 namespace BusinessLogicLayer.Services.Concrete
 {
@@ -11,10 +19,49 @@ namespace BusinessLogicLayer.Services.Concrete
     {
         public IProductRepository ProductRepository { get; set; }
 
-        public ProductService(ApplicationDbContext applicationDbContext, IMapper mapper) : base(mapper)
+        public ProductService(ApplicationDbContext applicationDbContext,
+            IMapper mapper, UnitOfWork unitOfWork) : base(mapper, unitOfWork)
         {
             ProductRepository = new ProductRepository(applicationDbContext);
             Repository = ProductRepository;
+        }
+
+        public async Task InsertAsync(CreateProductViewModel productModel)
+        {
+            StringBuilder photoURLBuilder = new();
+
+            List<string> uploadPaths = new();
+
+            if (!Directory.Exists(Constant.Path.PRODUCT_IMAGES))
+                Directory.CreateDirectory(Constant.Path.PRODUCT_IMAGES);
+
+            foreach (IFormFile photo in productModel.Photos)
+            {
+                string fileName = new StringBuilder()
+                    .Append(Path.GetFileNameWithoutExtension(photo.FileName))
+                    .Append('-')
+                    .Append(Guid.NewGuid().ToString().Replace("-", string.Empty))
+                    .Append(Path.GetExtension(photo.FileName)).ToString();
+
+                uploadPaths.Add(Path.Combine($"{Constant.Path.PRODUCT_IMAGES}", fileName));
+                photoURLBuilder.Append($"{fileName}|");
+            }
+
+            Product product = mapper.Map<Product>(productModel);
+
+            product.PhotoURL = photoURLBuilder.ToString();
+            product.GrantorID = UnitOfWork.UserService.GetCurrentUserId();
+
+            ProductRepository.Insert(product);
+
+            for (int i = 0; i < productModel.Photos.Count; i++)
+            {
+                string path = uploadPaths[i];
+                using (FileStream fileStream = new(path, FileMode.CreateNew))
+                {
+                    await productModel.Photos[i].CopyToAsync(fileStream);
+                }
+            }
         }
     }
 }
