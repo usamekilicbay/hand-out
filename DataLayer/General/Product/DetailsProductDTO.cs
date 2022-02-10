e6b990e88f512da0880b574c6c188ae35276baf5
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-10 20:48:36 +0300

    refactor: remove PhotoLinks property add PhotoURL instead in DetailsProductDTO

--- DataLayer/General/Product/DetailsProductDTO.cs
+++ DataLayer/General/Product/DetailsProductDTO.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace DataLayer.Product
 {
@@ -9,6 +7,7 @@ namespace DataLayer.Product
         public string Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
+        public string PhotoURL { get; set; }
         public string Address { get; set; }
         public string CategoryIcon { get; set; }
         public string CategoryName { get; set; }
@@ -16,11 +15,5 @@ namespace DataLayer.Product
         public string GrantorName { get; set; }
         public string GrantorPhotoURL { get; set; }
         public DateTime DatePublished { get; set; }
-        public List<string> PhotoLinks { get; private set; }
-
-        public DetailsProductDTO(string photoLink)
-        {
-            PhotoLinks = photoLink.Split('|').ToList();
-        }
     }
 }
