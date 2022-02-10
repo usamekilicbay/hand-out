5ac4d2d88997f3cbd2a5b5b6107cbb5c75cb36be
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-10 10:54:08 +0300

    refactor: add Display attribute for UserName property in UpdateUserViewModel Admin Area

--- hand-out/Areas/AdminPanel/Models/ViewModels/User/UpdateUserViewModel.cs
+++ hand-out/Areas/AdminPanel/Models/ViewModels/User/UpdateUserViewModel.cs
@@ -7,6 +7,7 @@ namespace hand_out.Areas.Admin.Models.ViewModels.User
     public class UpdateUserViewModel
     {
         public string Id { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
