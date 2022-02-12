66b2bb667e48d8cd53966ca45a98e41f479aca06
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-12 11:51:30 +0300

    refactor: override and fill GetAllWithRelations and GetByIdWithRelations methods in ProductRepository

--- DataAccessLayer/Repositories/Concrete/ProductRepository.cs
+++ DataAccessLayer/Repositories/Concrete/ProductRepository.cs
@@ -1,5 +1,10 @@
 ﻿using DataAccessLayer.Repositories.Abstract;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace DataAccessLayer.Repositories.Concrete
 {
@@ -7,6 +12,31 @@ namespace DataAccessLayer.Repositories.Concrete
     {
         public ProductRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
         {
+        }
+
+        public override List<Product> GetAllWithRelations()
+        {
+            return dbSet
+                .Include(p => p.Category)
+                .Include(p => p.Grantor)
+                .ToList();
+        }
+
+        public override List<Product> GetAllWithRelations(Expression<Func<Product, bool>> filter)
+        {
+            return dbSet
+                .Where(filter)
+                .Include(p => p.Category)
+                .Include(p => p.Grantor)
+                .ToList();
+        }
+
+        public override Product GetByIdWithRelations(int id)
+        {
+            return (Product)dbSet
+                .Where(p => p.Id == id)
+                .Include(p => p.Category)
+                .Include(p => p.Grantor);
         }
     }
 }
