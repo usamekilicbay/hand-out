cdd51e08003ec703a34be316e94ea4d34388f2cb
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-13 20:34:32 +0300

    feat: add MessageProductDTO and MessageProductViewModel

---
+++ DataLayer/General/Product/MessageProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DataLayer.Product
+{
+    public class MessageProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhotoURL { get; set; }
+        public string GrantorId { get; set; }
+        public string GrantorUserName { get; set; }
+        public string GrantorProfilePhotoURL { get; set; }
+    }
+}
