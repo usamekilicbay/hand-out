f9af2f1d5ad96a22582f24e1fd171bb3c7b98b9b
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-15 23:40:45 +0300

    feat: add DataType and DefaultValueSQL to Sidekick.NET

---
+++ Sidekick.NET/Constant/SQL/DataType.cs
@@ -0,0 +1,66 @@
+﻿namespace Sidekick.NET.Constant.SQL
+{
+    public static class DataType
+    {
+        public static class ExactNumeric
+        {
+            public const string BIGINT = "bigint";
+            public const string NUMERIC = "numeric";
+            public const string BIT = "bit";
+            public const string SMALLINT = "smallint";
+            public const string DECIMAL = "decimal";
+            public const string SMALLMONEY = "smallmoney";
+            public const string INT = "int";
+            public const string TINYINT = "tinyint";
+            public const string MONEY = "money";
+        }
+
+        public static class ApproximateNumeric
+        {
+            public const string FLOAT = "float";
+            public const string REAL = "real";
+        }
+
+        public static class DateAndTime
+        {
+            public const string DATE = "date";
+            public const string DATETIMEOFFSET = "datetimeoffset";
+            public const string DATETIME2 = "datetime2";
+            public const string SMALLDATETIME = "smalldatetime";
+            public const string DATETIME = "datetime";
+            public const string TIME = "time";
+        }
+
+        public static class CharacterString
+        {
+            public const string CHAR = "char";
+            public const string VARCHAR = "varchar";
+            public const string TEXT = "text";
+        }
+
+        public static class UnicodeCharacterString
+        {
+            public const string NCHAR = "nchar";
+            public const string NVARCHAR = "nvarchar";
+            public const string NTEXT = "ntext";
+        }
+
+        public static class BinaryString
+        {
+            public const string BINARY = "binary";
+            public const string VARBINARY = "varbinary";
+            public const string IMAGE = "image";
+        }
+
+        public static class OtherDataType
+        {
+            public const string CURSOR = "cursor";
+            public const string ROWVERSION = "rowversion";
+            public const string HIERARCHYID = "hierarchyid";
+            public const string UNIQUEIDENTIFIER = "uniqueidentifier";
+            public const string SQL_VARIANT = "sql_variant";
+            public const string XML    = "xml";
+            public const string TABLE = "table";
+        }
+    }
+}
---
+++ Sidekick.NET/Constant/SQL/DefaultValueSQL.cs
@@ -0,0 +1,9 @@
+﻿namespace Sidekick.NET.Constant.SQL
+{
+    public static class DefaultValueSQL
+    {
+        public const string NEW_ID = "NewId()";
+        public const string GET_DATE = "GetDate()";
+        public const string GET_UTC_DATE = "GetUtcDate()";
+    }
+}
