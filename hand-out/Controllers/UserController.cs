f631cf0a501fec443d2d408186180efe6f651115
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-16 00:26:54 +0300

    refactor: multiple important updates for UserController

--- hand-out/Controllers/UserController.cs
+++ hand-out/Controllers/UserController.cs
@@ -1,46 +1,70 @@
 ﻿using BusinessLogicLayer.Services.Abstract;
 using hand_out.Models;
-using DataLayer.ViewModels.Shared;
-using DataLayer.ViewModels.Product;
-using DataLayer.ViewModels.User;
+using DataLayer.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+using BusinessLogicLayer;
+using DataLayer.Category;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AutoMapper;
+using hand_out.Models.ViewModels.User;
+using hand_out.Models.ViewModels.Shared;
+using hand_out.Models.ViewModels.Product;
 
 namespace hand_out.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IUserService _userService;
+        private readonly IMapper _mapper;
 
-        public UserController(IUserService userService)
+        public UserController(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            _userService = userService;
+            _unitOfWork = unitOfWork;
+            _userService = unitOfWork.UserService;
+            _mapper = mapper;
         }
 
         [Authorize()]
-        [HttpGet]
-        public IActionResult Details()
+        public IActionResult Details(string id = "own")
         {
-            ProfileViewModel profileViewModel = new();
+            ProfileViewModel profileViewModel;
 
-            profileViewModel.UserDetailsViewModel = _userService.GetCurrentUserDetails<DetailsUserViewModel>();
-            profileViewModel.ProductCreateViewModel = new CreateProductViewModel();
+            if (id.Equals("own"))
+            {
+                ViewBag.IsOwnProfile = true;
+
+                profileViewModel = new(
+                    detailsUserViewModel: _mapper.Map<DetailsUserViewModel>(_userService.GetCurrentUserDetails()),
+                    createProductViewModel: new CreateProductViewModel());
+
+                ViewBag.Categories = new SelectList(_unitOfWork.CategoryService.GetAll<DropdownCategoryDTO>(), "Id", "Name");
+            }
+            else
+            {
+                ViewBag.IsOwnProfile = false;
+
+                profileViewModel = new(
+                    detailsUserViewModel: _mapper.Map<DetailsUserViewModel>(_userService.GetById<DetailsUserDTO>(id)),
+                    createProductViewModel: null);
+            }
 
             return View(profileViewModel);
         }
 
         public IActionResult Edit()
         {
-            return View(new ProfileEditViewModel
+            return View(new DetailsUserViewModel
             {
-                Name = "Usame Kilicbay",
-                Mail = "usame.kilicbay@protonmail.com",
-                Address = "Turkey",
-                ProfilePicURL = "https://media-exp1.licdn.com/dms/image/C5603AQEoe9--htofaA/profile-displayphoto-shrink_800_800/0/1576480070508?e=1644451200&v=beta&t=UBn25UjZZOMD16JzYKJtWGukEhFohlRe59w462JZy18",
+                //Name = "Usame Kilicbay",
+                //Mail = "usame.kilicbay@protonmail.com",
+                //Address = "Turkey",
+                //ProfilePicURL = "https://media-exp1.licdn.com/dms/image/C5603AQEoe9--htofaA/profile-displayphoto-shrink_800_800/0/1576480070508?e=1644451200&v=beta&t=UBn25UjZZOMD16JzYKJtWGukEhFohlRe59w462JZy18",
             });
         }
 
@@ -56,7 +80,7 @@ namespace hand_out.Controllers
             if (!ModelState.IsValid)
                 return View();
 
-            IdentityResult identityResult = _userService.SignUp(signUpUserViewModel, signUpUserViewModel.Password);
+            IdentityResult identityResult = _userService.SignUp(_mapper.Map<SignUpUserDTO>(signUpUserViewModel));
 
             if (!identityResult.Succeeded)
             {
@@ -75,14 +99,12 @@ namespace hand_out.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignIn(SignInUserViewModel signInUserViewModel)
+        public IActionResult SignIn(PasswordSignInUserViewModel signInUserViewModel)
         {
             if (!ModelState.IsValid)
                 return View();
 
-            SignInResult signInResult = _userService.PasswordSignIn(signInUserViewModel.Username,
-                                                            signInUserViewModel.Password,
-                                                            signInUserViewModel.RememberMe);
+            SignInResult signInResult = _userService.PasswordSignIn(_mapper.Map<PasswordSignInUserDTO>(signInUserViewModel));
 
             if (!signInResult.Succeeded)
             {
