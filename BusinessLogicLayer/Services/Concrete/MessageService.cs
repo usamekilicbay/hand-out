e5f19db83f1c2ad0c9c3685ea418eb1dec90b892
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-16 00:01:11 +0300

    feat: add IMessageService and MessageService to BusinessLogicLayer

---
+++ BusinessLogicLayer/Services/Concrete/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Services.Abstract;
+using DataAccessLayer;
+using DataAccessLayer.Repositories.Abstract;
+using DataAccessLayer.Repositories.Concrete;
+using EntityLayer.Concrete;
+
+namespace BusinessLogicLayer.Services.Concrete
+{
+    class MessageService : Service<Message>, IMessageService
+    {
+        private readonly IMessageRepository _messageRepository;
+
+        public MessageService(ApplicationDbContext applicationDbContext,IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
+        {
+            _messageRepository = new MessageRepository(applicationDbContext);
+            Repository = _messageRepository;
+        }
+    }
+}
