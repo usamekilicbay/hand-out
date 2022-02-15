afba71828d66d00a88b091a6b58c1c84534871d1
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-15 23:40:10 +0300

    feat: add Path to Sidekick.NET

---
+++ Sidekick.NET/Constant/Path/Path.cs
@@ -0,0 +1,18 @@
+﻿namespace Sidekick.NET.Constant
+{
+    public static class Path
+    {
+        public static string WWWROOT { get; private set; }
+        public static string IMAGES { get; private set; }
+        public static string PROFILE_IMAGES { get; private set; }
+        public static string PRODUCT_IMAGES { get; private set; }
+
+        public static void SetPaths(string wwwRootPath)
+        {
+            WWWROOT = wwwRootPath;
+            IMAGES = $"{WWWROOT}/images";
+            PROFILE_IMAGES = $"{IMAGES}/profile-images";
+            PRODUCT_IMAGES = $"{IMAGES}/product-images";
+        }
+    }
+}
