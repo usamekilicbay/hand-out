72c04dc6b856c16ee02420d1f2bb6b52cbae052b
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-15 23:59:13 +0300

    feat: add CreateProductViewModelValidator

---
+++ hand-out/Models/Validators/Product/CreateProductViewModelValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using hand_out.Models.ViewModels.Product;
+using static Sidekick.NET.Constant.Validation.Rule.Product;
+
+namespace hand_out.Models.Validators.Product
+{
+    public class CreateProductViewModelValidator : AbstractValidator<CreateProductViewModel>
+    {
+        public CreateProductViewModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .Length(Name.MIN_LENGTH, Name.MAX_LENGTH);
+
+            RuleFor(p => p.Details)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(Details.MAX_LENGTH);
+
+            RuleFor(p => p.Address)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(Address.MAX_LENGTH);
+        }
+    }
+}
