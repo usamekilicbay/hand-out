02ae980dfd93230848d094df235b9f821d70c394
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-16 00:06:08 +0300

    refactor: update GrantorName as GrantorUserName in DetailsProductDTO

--- DataLayer/General/Product/DetailsProductDTO.cs
+++ DataLayer/General/Product/DetailsProductDTO.cs
@@ -12,7 +12,7 @@ namespace DataLayer.Product
         public string CategoryIcon { get; set; }
         public string CategoryName { get; set; }
         public string GrantorId { get; set; }
-        public string GrantorName { get; set; }
+        public string GrantorUserName { get; set; }
         public string GrantorPhotoURL { get; set; }
         public DateTime DatePublished { get; set; }
     }
