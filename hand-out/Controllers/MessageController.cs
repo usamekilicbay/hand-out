a35bb52876ab5e5943af02bfa8662990fe818212
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-16 00:13:21 +0300

    refactor: fill MessageController

--- hand-out/Controllers/MessageController.cs
+++ hand-out/Controllers/MessageController.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using AutoMapper;
+using BusinessLogicLayer;
+using DataLayer.Product;
+using hand_out.Models.ViewModels.Message;
+using hand_out.Models.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace hand_out.Controllers
 {
     public class MessageController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public static string currentUserId;
+
+        public MessageController(IMapper mapper, IUnitOfWork unitOfWork)
         {
-            return View();
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            currentUserId = unitOfWork.UserService.GetCurrentUserId();
+        }
+
+        [HttpGet]
+        public IActionResult Index(int id = 0)
+        {
+            List<RecentChatViewModel> recentChatViewModels = new();
+            ActiveChatViewModel activeChatViewModel = new();
+            
+            if (id != 0)
+            {
+                MessageProductDTO messageProductDTO = _unitOfWork.ProductService.GetById<MessageProductDTO>(id);
+                activeChatViewModel.ProductMessageViewModel = _mapper.Map(messageProductDTO, new MessageProductViewModel(messageProductDTO.PhotoURL));
+            }
+
+            ChatViewModel chatViewModel = new(
+                recentChatViewModels: recentChatViewModels,
+                activeChatViewModel: activeChatViewModel
+                );
+
+            return View(chatViewModel);
         }
     }
 }
