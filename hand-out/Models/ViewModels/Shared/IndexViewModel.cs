51c200c527318127b5eee380da54597dc9da752a
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-15 23:58:42 +0300

    feat: add Shared View Models

---
+++ hand-out/Models/ViewModels/Shared/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using hand_out.Models.ViewModels.Category;
+using hand_out.Models.ViewModels.Product;
+using System.Collections.Generic;
+
+namespace hand_out.Models.ViewModels.Shared
+{
+    public class IndexViewModel
+    {
+        public List<ListProductViewModel> ListProductViewModels { get; private set; }
+        public List<ListCategoryViewModel> ListCategoryViewModels { get; private set; }
+
+        public IndexViewModel(List<ListProductViewModel> listProductViewModels, List<ListCategoryViewModel> listCategoryViewModels)
+        {
+            ListProductViewModels = listProductViewModels;
+            ListCategoryViewModels = listCategoryViewModels;
+        }
+    }
+}
---
+++ hand-out/Models/ViewModels/Shared/ProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using hand_out.Models.ViewModels.Product;
+using hand_out.Models.ViewModels.User;
+
+namespace hand_out.Models.ViewModels.Shared
+{
+    public class ProfileViewModel
+    {
+        public DetailsUserViewModel DetailsUserViewModel { get; private set; }
+        public CreateProductViewModel CreateProductViewModel { get; private set; }
+
+        public ProfileViewModel(DetailsUserViewModel detailsUserViewModel, CreateProductViewModel createProductViewModel)
+        {
+            DetailsUserViewModel = detailsUserViewModel;
+            CreateProductViewModel = createProductViewModel;
+        }
+    }
+}
