3390403829a8f55e59fb01eb7086715d5815ad7f
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-16 00:00:25 +0300

    refactor: update type of Id as int, GrantorName as GrantorUserName, PhotoLinks as PhotoURLs in DetailsProductViewModel

--- hand-out/Models/ViewModels/Product/DetailsProductViewModel.cs
+++ hand-out/Models/ViewModels/Product/DetailsProductViewModel.cs
@@ -6,21 +6,21 @@ namespace hand_out.Models.ViewModels.Product
 {
     public class DetailsProductViewModel
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
         public string Address { get; set; }
         public string CategoryIcon { get; set; }
         public string CategoryName { get; set; }
         public string GrantorId { get; set; }
-        public string GrantorName { get; set; }
+        public string GrantorUserName { get; set; }
         public string GrantorPhotoURL { get; set; }
         public DateTime DatePublished { get; set; }
-        public List<string> PhotoLinks { get; private set; }
+        public List<string> PhotoURLs { get; private set; }
 
         public DetailsProductViewModel(string photoLink)
         {
-            PhotoLinks = photoLink.Split('|').ToList();
+            PhotoURLs = photoLink.Split('|').ToList();
         }
     }
 }
