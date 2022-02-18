11152107c0c8ece2066bb01d92a5c903690986eb
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-18 12:14:04 +0300

    feat: migration of Id changes, relations between Product and Message

--- DataAccessLayer/Migrations/20220218090832_MessageAndProductRelationAndChangeAllIDAsId.Designer.cs
+++ DataAccessLayer/Migrations/20220218090832_MessageAndProductRelationAndChangeAllIDAsId.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220218090832_MessageAndProductRelationAndChangeAllIDAsId")]
+    partial class MessageAndProductRelationAndChangeAllIDAsId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccessLayer/Migrations/20220218090832_MessageAndProductRelationAndChangeAllIDAsId.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class MessageAndProductRelationAndChangeAllIDAsId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_GrantorID",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryID",
+                table: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "GrantorID",
+                table: "Product",
+                newName: "GrantorId");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Product",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_GrantorID",
+                table: "Product",
+                newName: "IX_Product_GrantorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CategoryID",
+                table: "Product",
+                newName: "IX_Product_CategoryId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Message",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_ProductId",
+                table: "Message",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Product_ProductId",
+                table: "Message",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_GrantorId",
+                table: "Product",
+                column: "GrantorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Product_ProductId",
+                table: "Message");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_GrantorId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Message_ProductId",
+                table: "Message");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Message");
+
+            migrationBuilder.RenameColumn(
+                name: "GrantorId",
+                table: "Product",
+                newName: "GrantorID");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Product",
+                newName: "CategoryID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_GrantorId",
+                table: "Product",
+                newName: "IX_Product_GrantorID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                newName: "IX_Product_CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_GrantorID",
+                table: "Product",
+                column: "GrantorID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryID",
+                table: "Product",
+                column: "CategoryID",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
