6f7280733a19249448382d659a0499b098fb0318
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-20 21:47:59 +0300

    feat: add SaveMessageDTO

---
+++ DataLayer/General/Message/CreateMessageDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataLayer.General.Message
+{
+    public class CreateMessageDTO
+    {
+        public int ProductId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Context { get; set; }
+    }
+}
