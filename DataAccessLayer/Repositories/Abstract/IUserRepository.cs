f839b00c494e1e0d07443a5a500297256d08b842
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-21 12:12:50 +0300

    refactor: remove public access modifier from IUserRepository

--- DataAccessLayer/Repositories/Abstract/IUserRepository.cs
+++ DataAccessLayer/Repositories/Abstract/IUserRepository.cs
@@ -7,16 +7,16 @@ namespace DataAccessLayer.Repositories.Abstract
 {
     public interface IUserRepository : IRepository<User>
     {
-        public UserManager<User> UserManager { get; set; }
-        public SignInManager<User> SignInManager { get; set; }
-        public IHttpContextAccessor HttpContextAccessor { get; set; }
+        UserManager<User> UserManager { get; set; }
+        SignInManager<User> SignInManager { get; set; }
+        IHttpContextAccessor HttpContextAccessor { get; set; }
 
-        public string GetCurrentUserId();
-        public User GetCurrentUser();
-        public SignInResult PasswordSignIn(PasswordSignInUserDTO passwordSignInUserDTO);
-        public IdentityResult SignUp(User user, string password);
-        public void SignOut();
-        public User GetByIdWithMessages(string id);
-        public User GetByIdWithMessages(string id, int productId);
+        string GetCurrentUserId();
+        User GetCurrentUser();
+        SignInResult PasswordSignIn(PasswordSignInUserDTO passwordSignInUserDTO);
+        IdentityResult SignUp(User user, string password);
+        void SignOut();
+        User GetByIdWithMessages(string id);
+        User GetByIdWithMessages(string id, int productId);
     }
 }
