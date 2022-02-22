fa00d7339329c80122bfc2051eeae068cb6c3b5a
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-22 12:48:36 +0300

    feat: add Chat Rules to Sidekick.NET

---
+++ Sidekick.NET/Constant/Validation/Rule/Chat.cs
@@ -0,0 +1,18 @@
+﻿using Sidekick.NET.Constant.SQL;
+
+namespace Sidekick.NET.Constant.Validation.Rule
+{
+    public class Chat
+    {
+        public static class Id
+        {
+            public const string DEFAULT_VALUE_SQL = DefaultValueSQL.NEW_ID;
+        }
+
+        public static class DateCreated
+        {
+            public const string DATA_TYPE = DataType.DateAndTime.SMALLDATETIME;
+            public const string DEFAULT_VALUE_SQL = DefaultValueSQL.GET_DATE;
+        }
+    }
+}
