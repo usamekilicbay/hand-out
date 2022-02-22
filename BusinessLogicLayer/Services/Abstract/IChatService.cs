f1f802239602a3c3e9e7dda544264293678becc9
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-22 14:53:10 +0300

    feat: create IChatService and ChatService

---
+++ BusinessLogicLayer/Services/Abstract/IChatService.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLogicLayer.Services.Abstract
+{
+    public interface IChatService : IService<Chat>
+    {
+        IChatRepository ChatRepository { get; set; }
+    }
+}
---
+++ BusinessLogicLayer/Services/Concrete/ChatService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Services.Abstract;
+using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLogicLayer.Services.Concrete
+{
+    public class ChatService : Service<Chat>, IChatService
+    {
+        public IChatRepository ChatRepository { get; set; }
+
+        public ChatService(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
+        {
+        }
+    }
+}
