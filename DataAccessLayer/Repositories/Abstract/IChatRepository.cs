62c98a97353a286b2eacc3a6ac5a4822aaea9760
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-22 14:46:09 +0300

    feat: create IChatRepository and ChatRepository

---
+++ DataAccessLayer/Repositories/Abstract/IChatRepository.cs
@@ -0,0 +1,8 @@
+﻿using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Repositories.Abstract
+{
+    public interface IChatRepository : IRepository<Chat>
+    {
+    }
+}
---
+++ DataAccessLayer/Repositories/Concrete/ChatRepository.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Repositories.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Repositories.Concrete
+{
+    public class ChatRepository : Repository<Chat>, IChatRepository
+    {
+        public ChatRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+
+        }
+
+        public override List<Chat> GetAllWithRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Chat> GetAllWithRelations(Expression<Func<Chat, bool>> filter)
+            => dbSet
+            .Where(filter)
+            .Include(c => c.Product)
+            .Include(c => c.GrantorParticipant)
+            .Include(c => c.NeedyParticipant)
+            .ToList();
+    }
+}
