9bbc298d9eeb826c2c88d345f16e9c25b9855819
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-22 12:46:24 +0300

    feat: add Chat Entity

---
+++ EntityLayer/Concrete/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityLayer.Concrete
+{
+    public class Chat
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string NeedyParticipantId { get; set; }
+        public User NeedyParticipant { get; set; }
+        public User GrantorParticipantId { get; set; }
+        public User GrantorParticipant { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
