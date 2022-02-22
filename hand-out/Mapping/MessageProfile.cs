04c4e80516874a8e351f7c4a8e6f06dae3a152b8
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-22 09:10:31 +0300

    feat: create MessageProfile

---
+++ hand-out/Mapping/MessageProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DataLayer.General.Message;
+using EntityLayer.Concrete;
+
+namespace hand_out.Mapping
+{
+    public class MessageProfile : Profile
+    {
+        public MessageProfile()
+        {
+            #region Entity <---> DTO
+            CreateMap<Message, CreateMessageDTO>().ReverseMap();
+            #endregion
+        }
+    }
+}
