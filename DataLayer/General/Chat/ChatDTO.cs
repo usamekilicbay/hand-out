13269054580025d373b9b13606820cd6a051ab96
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-23 23:10:58 +0300

    feat: create ChatDTO

---
+++ DataLayer/General/Chat/ChatDTO.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.General.Message;
+using System.Collections.Generic;
+
+namespace DataLayer.General.Chat
+{
+    public class ChatDTO
+    {
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPhotoURL { get; set; }
+        public string GrantorParticipantId { get; set; }
+        public string GrantorParticipantUserName { get; set; }
+        public string GrantorParticipantPhotoURL { get; set; }
+        public List<MessageDTO> Messages { get; set; }
+    }
+}
