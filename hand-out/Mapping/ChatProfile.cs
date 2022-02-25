02e0efd4bab01ef38f7eeebdb5dadbadb4df3bc6
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-25 11:46:41 +0300

    feat: add ChatProfile

---
+++ hand-out/Mapping/ChatProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DataLayer.General.Chat;
+using EntityLayer.Concrete;
+using hand_out.Models.ViewModels.Chat;
+
+namespace hand_out.Mapping
+{
+    public class ChatProfile : Profile
+    {
+        public ChatProfile()
+        {
+            #region Entity <---> DTO
+            CreateMap<Chat, ChatDTO>().ReverseMap();
+            CreateMap<Chat, CreateChatDTO>().ReverseMap();
+            #endregion
+
+            #region ViewModel <---> DTO
+            CreateMap<ChatViewModel, ChatDTO>().ReverseMap();
+            #endregion
+        }
+    }
+}
