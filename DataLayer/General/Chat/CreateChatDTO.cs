6130e49f3a6712cfb9c36e2fe043d4460fcb7c28
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-25 11:40:10 +0300

    feat: create CreateChatDTO

---
+++ DataLayer/General/Chat/CreateChatDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLayer.General.Chat
+{
+    public class CreateChatDTO
+    {
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public string GrantorParticipantId { get; set; }
+    }
+}
