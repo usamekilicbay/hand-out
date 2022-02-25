08c646aa28eae7353294ceb73affb0775615a7c7
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-25 11:42:24 +0300

    refactor: add virtual keyword to relation properties

--- EntityLayer/Concrete/Chat.cs
+++ EntityLayer/Concrete/Chat.cs
@@ -8,11 +8,11 @@ namespace EntityLayer.Concrete
         public string Id { get; set; }
         public DateTime DateCreated { get; set; }
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public virtual Product Product { get; set; }
         public string GrantorParticipantId { get; set; }
-        public User GrantorParticipant { get; set; }
+        public virtual User GrantorParticipant { get; set; }
         public string NeedyParticipantId { get; set; }
-        public User NeedyParticipant { get; set; }
+        public virtual User NeedyParticipant { get; set; }
         public virtual ICollection<Message> Messages { get; set; }
     }
 }
