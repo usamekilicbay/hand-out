3288108de53dc30ab84fe7b95ab1d5771cd483d1
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-26 23:37:35 +0300

    feat: create ListChatViewModel

---
+++ hand-out/Models/ViewModels/Chat/ListChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace hand_out.Models.ViewModels.Chat
+{
+    public class ListChatViewModel
+    {
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPhotoURL { get; set; }
+        public string SenderParticipantId { get; set; }
+        public string ReceiverParticipantId { get; set; }
+        public string ReceiverParticipantUserName { get; set; }
+        public string ReceiverParticipantPhotoURL { get; set; }
+    }
+}
