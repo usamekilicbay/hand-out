a2f22799faf5515ecc7afa1c6ea0a5d54378a0a1
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-27 21:45:12 +0300

    feat: add ChatViewModel

---
+++ hand-out/Models/ViewModels/Chat/ChatViewModel.cs
@@ -0,0 +1,17 @@
+﻿using hand_out.Models.ViewModels.Message;
+using System.Collections.Generic;
+
+namespace hand_out.Models.ViewModels.Chat
+{
+    public class ChatViewModel
+    {
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPhotoURL { get; set; }
+        public string ReceiverParticipantId { get; set; }
+        public string ReceiverParticipantUserName { get; set; }
+        public string ReceiverParticipantPhotoURL { get; set; }
+        public List<MessageViewModel> Messages { get; set; }
+    }
+}
