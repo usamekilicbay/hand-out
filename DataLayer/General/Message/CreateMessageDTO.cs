6b6b680b53532c2c179bb7e6cf37e67e61eca009
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-27 21:35:02 +0300

    refactor: add ChatId property to CreateMessageDTO

--- DataLayer/General/Message/CreateMessageDTO.cs
+++ DataLayer/General/Message/CreateMessageDTO.cs
@@ -1,9 +1,8 @@
-﻿using System;
-
-namespace DataLayer.General.Message
+﻿namespace DataLayer.General.Message
 {
     public class CreateMessageDTO
     {
+        public string ChatId { get; set; }
         public int ProductId { get; set; }
         public string ReceiverId { get; set; }
         public string Context { get; set; }
