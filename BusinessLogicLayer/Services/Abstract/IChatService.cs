bec8afbf77010b5094a802220aa4cc8b2f93866d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-27 21:01:59 +0300

    refactor: add Insert and GetAllWithRelations method signs to IChatService

--- BusinessLogicLayer/Services/Abstract/IChatService.cs
+++ BusinessLogicLayer/Services/Abstract/IChatService.cs
@@ -1,10 +1,17 @@
 ﻿using DataAccessLayer.Repositories.Abstract;
+using DataLayer.General.Chat;
 using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace BusinessLogicLayer.Services.Abstract
 {
     public interface IChatService : IService<Chat>
     {
         IChatRepository ChatRepository { get; set; }
+
+        ChatDTO Insert(CreateChatDTO createChatDTO);
+        List<ChatDTO> GetAllWithRelations(Expression<Func<Chat, bool>> filter);
     }
 }
