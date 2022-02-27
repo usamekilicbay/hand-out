db5aa02f3feab112010b0dbdbcc3979449fec046
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-27 21:50:41 +0300

    feat: create MessageViewModel

---
+++ hand-out/Models/ViewModels/Message/MessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hand_out.Models.ViewModels.Message
+{
+    public class MessageViewModel
+    {
+        public string Context { get; set; }
+        public string SenderId { get; set; }
+        public DateTime DateSent { get; set; }
+        public bool IsYourMessage { get; set; }
+    }
+}
