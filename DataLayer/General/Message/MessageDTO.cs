96ec6cb28489ab11a14ba2a2a455c3aa37e4d694
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-27 21:51:38 +0300

    feat: create MessageDTO

---
+++ DataLayer/General/Message/MessageDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataLayer.General.Message
+{
+    public class MessageDTO
+    {
+        public string Context { get; set; }
+        public string SenderId;
+        public DateTime DateSent { get; set; }
+    }
+}
