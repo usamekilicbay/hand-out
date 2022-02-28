04911ad978dda27483505982921084dbe8ddea99
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-02-28 23:24:16 +0300

    feat: create UpdateUserDTO

---
+++ DataLayer/General/User/UpdateUserDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using static Sidekick.NET.Types;
+
+namespace DataLayer.General.User
+{
+    public class UpdateUserDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public IFormFile PhotoFile { get; set; }
+        public DateTime LastSeen { get; set; }
+        public UserStatus Status { get; set; }
+    }
+}
