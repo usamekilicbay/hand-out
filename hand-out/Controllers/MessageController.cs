efdb092956cef4efb62946bbc48697619dc6f4ac
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-01 21:54:11 +0300

    refactor: multiple updates in MessageController Chat System is Done

--- hand-out/Controllers/MessageController.cs
+++ hand-out/Controllers/MessageController.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using BusinessLogicLayer;
-using DataLayer.Product;
-using hand_out.Models.ViewModels.Message;
-using hand_out.Models.ViewModels.Product;
+using DataLayer.General.Chat;
+using DataLayer.General.Message;
+using EntityLayer.Concrete;
+using hand_out.Models.ViewModels.Chat;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace hand_out.Controllers
 {
@@ -24,21 +26,83 @@ namespace hand_out.Controllers
         [HttpGet]
         public IActionResult Index(int id = 0)
         {
-            List<RecentChatViewModel> recentChatViewModels = new();
-            ActiveChatViewModel activeChatViewModel = new();
-            
-            if (id != 0)
+            ViewBag.ProductId = id;
+            return View();
+        }
+
+        public List<ListChatViewModel> GetChats()
+        {
+            List<ChatDTO> chatDTOs = _unitOfWork.ChatService.GetAllWithRelations(
+                c => c.GrantorParticipantId == currentUserId
+                || c.NeedyParticipantId == currentUserId);
+
+            List<ListChatViewModel> chatlist = new();
+
+            chatDTOs.ForEach(x => chatlist.Add(new ListChatViewModel()
             {
-                MessageProductDTO messageProductDTO = _unitOfWork.ProductService.GetById<MessageProductDTO>(id);
-                activeChatViewModel.ProductMessageViewModel = _mapper.Map(messageProductDTO, new MessageProductViewModel(messageProductDTO.PhotoURL));
+                Id = x.Id,
+                ProductId = x.ProductId,
+                ProductName = x.ProductName,
+                ProductPhotoURL = x.ProductPhotoURL,
+                SenderParticipantId = currentUserId,
+                ReceiverParticipantId = x.ReceiverParticipantId,
+                ReceiverParticipantUserName = x.ReceiverParticipantUserName,
+                ReceiverParticipantPhotoURL = x.ReceiverParticipantPhotoURL
+            }));
+
+            return chatlist;
+        }
+
+        public ChatViewModel GetActiveChatViewModel(int productId = 3)
+        {
+            Product product = _unitOfWork.ProductService.GetAllWithRelations<Product>(p => p.Id == productId).FirstOrDefault();
+
+            ChatDTO chatDTO = GetChatDTO(productId);
+
+            if (chatDTO != null)
+            {
+                ChatViewModel chatview = _mapper.Map<ChatViewModel>(chatDTO);
+                chatview.Messages.ForEach(x => x.IsYourMessage = x.SenderId == currentUserId);
+                return chatview;
             }
 
-            ChatViewModel chatViewModel = new(
-                recentChatViewModels: recentChatViewModels,
-                activeChatViewModel: activeChatViewModel
-                );
+            return new()
+            {
+                ProductId = product.Id,
+                ProductName = product.Name,
+                ProductPhotoURL = product.PhotoURL,
+                ReceiverParticipantId = product.GrantorId,
+                ReceiverParticipantUserName = product.Grantor.UserName,
+                ReceiverParticipantPhotoURL = product.Grantor.ProfilePhotoURL
+            };
+        }
 
-            return View(chatViewModel);
+        public void SaveMessage(CreateMessageDTO createMessageDTO)
+        {
+            ChatDTO chatDTO = GetChatDTO(createMessageDTO.ProductId);
+
+            createMessageDTO.ChatId = chatDTO == null
+                ? CreateChat(createMessageDTO.ProductId, createMessageDTO.ReceiverId).Id
+                : chatDTO.Id;
+
+            _unitOfWork.MessageService.Insert(createMessageDTO);
+        }
+
+        private ChatDTO GetChatDTO(int productId)
+          => _unitOfWork.ChatService.GetAllWithRelations(
+              c => c.ProductId == productId &&
+              c.GrantorParticipantId == currentUserId ||
+              c.NeedyParticipantId == currentUserId).FirstOrDefault();
+
+        public ChatDTO CreateChat(int productId, string grantorParticipantId)
+        {
+            CreateChatDTO createChatDTO = new()
+            {
+                ProductId = productId,
+                GrantorParticipantId = grantorParticipantId,
+            };
+
+            return _unitOfWork.ChatService.Insert(createChatDTO);
         }
     }
 }
