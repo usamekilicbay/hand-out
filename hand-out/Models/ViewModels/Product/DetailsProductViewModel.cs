f7f252cff0d97369aa97be456d2bb9c86fe56611
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-03 20:02:14 +0300

    refactor: change GrantorPhotoURL as GrantorProfilePhotoURL in DetailsProductViewModel and DetailsProductDTO

--- hand-out/Models/ViewModels/Product/DetailsProductViewModel.cs
+++ hand-out/Models/ViewModels/Product/DetailsProductViewModel.cs
@@ -14,7 +14,7 @@ namespace hand_out.Models.ViewModels.Product
         public string CategoryName { get; set; }
         public string GrantorId { get; set; }
         public string GrantorUserName { get; set; }
-        public string GrantorPhotoURL { get; set; }
+        public string GrantorProfilePhotoURL { get; set; }
         public DateTime DatePublished { get; set; }
         public List<string> PhotoURLs { get; private set; }
 
