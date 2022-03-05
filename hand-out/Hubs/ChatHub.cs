639eac618a451aecedb459dc1c601f4b5416bfb1
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-05 19:44:44 +0300

    refactor: multiple updates in ChatHub Chat System is done

--- hand-out/Hubs/ChatHub.cs
+++ hand-out/Hubs/ChatHub.cs
@@ -1,30 +1,22 @@
 ﻿using BusinessLogicLayer;
-using BusinessLogicLayer.Services.Abstract;
 using hand_out.Controllers;
 using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace hand_out.Hubs
 {
     public class ChatHub : Hub
     {
-        private readonly IUserIdProvider _userIdProvider;
-        private readonly IUserService _userService;
         private readonly IUnitOfWork _unitOfWork;
 
-        public static Hashtable OnlineUsers = new();
+        private static Dictionary<string, string> OnlineUsers = new();
 
-        public ChatHub(IUserIdProvider userIdProvider, IUserService userService, IUnitOfWork unitOfWork)
+        public ChatHub(IUnitOfWork unitOfWork)
         {
-            _userIdProvider = userIdProvider;
-            _userService = userService;
             _unitOfWork = unitOfWork;
         }
-
 
         public override Task OnConnectedAsync()
         {
@@ -41,8 +33,8 @@ namespace hand_out.Hubs
 
         public async void SendMessageAsync(string receiverId, string message)
         {
-            await Clients.Client(OnlineUsers[receiverId].ToString()).SendAsync("receiveMessage", message);
-            //await Clients.User(receiverId).SendAsync("receiveMessage", receiverId, message);
+            if (OnlineUsers.ContainsKey(receiverId))
+                await Clients.Client(OnlineUsers[receiverId]).SendAsync("receiveMessage", message);
         }
     }
 }
