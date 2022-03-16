955e86cbc175f8de88f66159584a17e53148bd4a
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-16 09:52:29 +0300

    refactor: add GetUserDetails, GetUserProfile, GetCurrentUserProfile method signs to IUserService

--- BusinessLogicLayer/Services/Abstract/IUserService.cs
+++ BusinessLogicLayer/Services/Abstract/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Repositories.Abstract;
+using DataLayer.Shared;
 using DataLayer.User;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Http;
@@ -10,8 +11,11 @@ namespace BusinessLogicLayer.Services.Abstract
     {
         IUserRepository UserRepository { get; set; }
 
+        public DetailsUserDTO GetUserDetails(string userId);
+        public ProfileDTO GetUserProfile(string userId);
         string GetCurrentUserId();
         DetailsUserDTO GetCurrentUserDetails();
+        public ProfileDTO GetCurrentUserProfile();
         void UpdateProfilePhoto(IFormFile photo);
         SignInResult PasswordSignIn(PasswordSignInUserDTO passwordSignInUserDTO);
         IdentityResult SignUp(SignUpUserDTO signUpUserDTO);
