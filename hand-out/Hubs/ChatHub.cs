5dee4c3dd63c5d6f7e39aa7feeb799fe1ab7e402
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-16 09:54:04 +0300

    refactor: add _currentUserId and ChatSendMessageDTO parameter in SendMessageAsync method to ChatHub

--- hand-out/Hubs/ChatHub.cs
+++ hand-out/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLogicLayer;
+using DataLayer.General.Chat;
 using hand_out.Controllers;
 using Microsoft.AspNetCore.SignalR;
 using System;
@@ -10,31 +11,32 @@ namespace hand_out.Hubs
     public class ChatHub : Hub
     {
         private readonly IUnitOfWork _unitOfWork;
-
         private static Dictionary<string, string> OnlineUsers = new();
+        private string _currentUserId;
 
         public ChatHub(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _currentUserId = unitOfWork.UserService.GetCurrentUserId();
         }
 
         public override Task OnConnectedAsync()
         {
-            OnlineUsers.Add(MessageController.currentUserId, Context.ConnectionId);
+            OnlineUsers.Add(_currentUserId, Context.ConnectionId);
             return Clients.All.SendAsync("gg", "");
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            OnlineUsers.Remove(MessageController.currentUserId);
+            OnlineUsers.Remove(_currentUserId);
 
             return Clients.All.SendAsync("ff", "");
         }
 
-        public async void SendMessageAsync(string receiverId, string message)
+        public async void SendMessageAsync(ChatSendMessageDTO chatSendMessageDTO, string message)
         {
-            if (OnlineUsers.ContainsKey(receiverId))
-                await Clients.Client(OnlineUsers[receiverId]).SendAsync("receiveMessage", message);
+            if (OnlineUsers.ContainsKey(chatSendMessageDTO.ReceiverId))
+                await Clients.Client(OnlineUsers[chatSendMessageDTO.ReceiverId]).SendAsync("receiveMessage", message, chatSendMessageDTO);
         }
     }
 }
