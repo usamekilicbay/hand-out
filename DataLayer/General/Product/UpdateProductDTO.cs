d02c83d2008d97f94473f60f988e419f11ea7140
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-19 23:28:21 +0300

    feat: create UpdateProductDTO and UpdateProductViewModel

---
+++ DataLayer/General/Product/UpdateProductDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static Sidekick.NET.Types;
+
+namespace DataLayer.Product
+{
+    public class UpdateProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Address { get; set; }
+        public string PhotoURL { get; set; }
+        public List<IFormFile> Photos { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public ProductStatus Status { get; set; }
+    }
+}
---
+++ hand-out/Models/ViewModels/Product/UpdateProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using static Sidekick.NET.Types;
+
+namespace hand_out.Models.ViewModels.Product
+{
+    public class UpdateProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Address { get; set; }
+        public List<string> PhotoURLs { get; set; }
+        public List<IFormFile> Photos { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public ProductStatus Status { get; set; }
+
+        public UpdateProductViewModel(string photoURL)
+        {
+            PhotoURLs = photoURL.Split("|").ToList();
+        }
+
+        public UpdateProductViewModel()
+        {
+
+        }
+    }
+}
