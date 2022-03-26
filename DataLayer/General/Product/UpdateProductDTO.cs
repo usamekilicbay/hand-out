db931fe2c8f85e86d168e0fba1f3ba8d66b5a7e8
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-03-26 13:02:50 +0300

    feat: add PhotoURLs property and SetPhotoURLs method to UpdateProductDTO

--- DataLayer/General/Product/UpdateProductDTO.cs
+++ DataLayer/General/Product/UpdateProductDTO.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using static Sidekick.NET.Types;
 
 namespace DataLayer.Product
@@ -12,9 +13,15 @@ namespace DataLayer.Product
         public string Details { get; set; }
         public string Address { get; set; }
         public string PhotoURL { get; set; }
+        public List<string> PhotoURLs { get; set; }
         public List<IFormFile> Photos { get; set; }
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
         public ProductStatus Status { get; set; }
+
+        public void SetPhotoURLs()
+        {
+            PhotoURLs = PhotoURL.Split("|").ToList();
+        }
     }
 }
